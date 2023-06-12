87514325d27b7efc7494a51728dfb05da09acbee
yycc0526 <yucc.coke@gmail.com>
2023-06-12 17:51:45 +0800

    update: 优化

--- web/src/components/ChatBox.tsx
+++ web/src/components/ChatBox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import SendMessageInput from "./SendMessageInput";
 import MessageBox from "./MessageBox";
 import Introduction from "./Introduction";
